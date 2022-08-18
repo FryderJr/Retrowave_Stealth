f22f47de0af957cd01572b5bb4b76fe59c40a408
Vladimir Iagodkin <redcarpet@mail.ru>
2022-08-18 15:59:06 +0300

    feat: material with opacity mask added

--- Source/Retrowave_Stealth/Private/Player/RSBaseCharacter.cpp
+++ Source/Retrowave_Stealth/Private/Player/RSBaseCharacter.cpp
@@ -85,12 +85,11 @@ void ARSBaseCharacter::MoveRight(float Amount)
 void ARSBaseCharacter::OnCameraCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
     UE_LOG(RSBaseCharacter_LOG, Display, TEXT("Camera touched: %s"), *UKismetSystemLibrary::GetDisplayName(OtherComp));
-    //OtherComp->SetOwnerNoSee(true);
-    OtherComp->SetVisibility(false);
+    //OtherComp->SetVisibility(false);
 }
 
 void ARSBaseCharacter::OnCameraCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-    OtherComp->SetVisibility(true);
+    //OtherComp->SetVisibility(true);
 }
 
