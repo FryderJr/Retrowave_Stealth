52d8b6377869b0b8db52208d920f7af08194269e
Vladimir Iagodkin <redcarpet@mail.ru>
2022-09-12 18:17:26 +0300

    feat: library show/hide logic

--- Source/Retrowave_Stealth/Private/UI/RSLibraryWidget.cpp
+++ Source/Retrowave_Stealth/Private/UI/RSLibraryWidget.cpp
@@ -12,8 +12,10 @@ void URSLibraryWidget::NativeOnInitialized()
     const auto GI = GetWorld()->GetGameInstance<URSGameInstance>();
     if (GI)
     {
-        OpendKnowleges = GI->GetInfoPoints();
+        //OpendKnowleges = GI->GetInfoPoints();
     }
+
+    OpendKnowleges = 3;
 
     if (NextKnowlege)
     {
@@ -25,11 +27,13 @@ void URSLibraryWidget::OnNextKnowlege()
 {
     if (OpendKnowleges == 0) return;
     const auto NextIndex = ++CurrentKnowlegeIndex % OpendKnowleges;
+    UE_LOG(LogTemp, Display, TEXT("Next"));
     ShowCurrentKnowlege(NextIndex);
 }
 
 void URSLibraryWidget::ShowCurrentKnowlege(int32 Index)
 {
     if (!FoundKnowledges.IsValidIndex(Index)) return;
+    UE_LOG(LogTemp, Display, TEXT("slide∆ %i"), Index);
     CurrentKnowlege = FoundKnowledges[Index];
 }
--- Source/Retrowave_Stealth/Public/UI/RSLibraryWidget.h
+++ Source/Retrowave_Stealth/Public/UI/RSLibraryWidget.h
@@ -17,12 +17,18 @@ class RETROWAVE_STEALTH_API URSLibraryWidget : public UUserWidget
 public:
     virtual void NativeOnInitialized() override;
 
+    UFUNCTION(BlueprintCallable)
+    UTexture2D* GetCurrentKnowlege() const { return CurrentKnowlege; };
+
 protected:
     UPROPERTY(meta = (BindWidget))
     UButton* NextKnowlege;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Knowledges")
     TArray<UTexture2D*> FoundKnowledges;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Knowledges")
+    UTexture2D* DefaultImage;
 	
 private:
     int32 OpendKnowleges{0};
